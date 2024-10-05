1991594dbddd233c02866c145e917ef03d819b0e
dezhizhang <1541609448@qq.com>
2024-10-05 23:15:49 +0800

    feat: 学词鸭在线英词练习平台

--- components/NavBar/index.tsx
+++ components/NavBar/index.tsx
@@ -5,8 +5,23 @@
  * :copyright: (c) 2024, Xiaozhi
  * :date created: 2024-10-05 22:19:33
  * :last editor: 张德志
- * :date last edited: 2024-10-05 22:21:49
+ * :date last edited: 2024-10-05 23:14:36
  */
+import Image from "next/image";
 export default function NavBar() {
-  return <div>顶部导航</div>;
+  return (
+    <div className="h-16 bg-blue-600 flex px-8 items-center">
+      <div className="pr-6 flex items-center flex-1">
+        <Image
+          width={120}
+          height={64}
+          src="https://typingfun.cn/assets/img/daziya-logo.svg"
+          alt=""
+        />
+        <span className="h-9 w-1 bg-yellow-300" />
+        <h2>免费在线学词平台</h2>
+      </div>
+      <div className="111">2222</div>
+    </div>
+  );
 }
