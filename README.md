c0e30f7b18a98727b4418dbe9f230c826c039e07
dezhizhang <1541609448@qq.com>
2024-10-05 22:05:04 +0800

    feat: 项目初始化

---
+++ README.md
@@ -0,0 +1,14 @@
+
+# 学词鸭
+
+First, run the development server:
+
+```bash
+npm run dev
+# or
+yarn dev
+# or
+pnpm dev
+# or
+bun dev
+```
---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+/*
+ * :file description: 
+ * :name: /xueciya/app/page.tsx
+ * :author:张德志
+ * :copyright: (c) 2024, Xiaozhi
+ * :date created: 2024-10-05 22:01:24
+ * :last editor: 张德志
+ * :date last edited: 2024-10-05 22:03:28
+ */
+
+export default function Home() {
+  return (
+    <div>hello</div>
+  );
+}
